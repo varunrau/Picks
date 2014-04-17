60f49b788d6a4ba2ed6ba60311c6d31a5424a810
varunrau <varunrau@gmail.com>
2014-04-16 23:28:12 -0700

    Initial Commit

---
+++ Picks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Picks
+//
+//  Created by Varun Rau on 4/16/14.
+//  Copyright (c) 2014 Rafael and Rau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ PicksTests/PicksTests.m
@@ -0,0 +1,34 @@
+//
+//  PicksTests.m
+//  PicksTests
+//
+//  Created by Varun Rau on 4/16/14.
+//  Copyright (c) 2014 Rafael and Rau. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PicksTests : XCTestCase
+
+@end
+
+@implementation PicksTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
