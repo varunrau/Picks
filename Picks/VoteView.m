0be962f2113066ee7ee216e1abf08f1e540290b2
varunrau <varunrau@gmail.com>
2014-04-18 15:25:38 -0700

    removed unnecessary method

--- Picks/VoteView.m
+++ Picks/VoteView.m
@@ -57,13 +57,4 @@
     return cv;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
-
 @end
